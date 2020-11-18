049eb1e35ab1796a0ff8ce594c01ca3c2e07defb
Brianne <brianne.ewell1@gmail.com>
2020-11-17 19:46:46 -0700

    adding questions to employee

--- Employee.js
+++ Employee.js
@@ -1,1 +1,61 @@
 // TODO: Write code to define and export the Employee class
+/*name
+id
+email
+getName()
+getId()
+getEmail()
+getRole() // Returns 'Employee'*/
+
+//Global Variables
+//Import inquirer and node modules
+const { prompt } = require('inquirer');
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+
+const questions = [
+    {
+		//Name
+		type: 'input',
+		message: 'Enter employee first and last name',
+		name: 'name',
+		validate: function(name) {
+			let pass = name.match(/^[a-zA-Z]+ [a-zA-Z]+$/g);
+			if (pass) {
+				return true;
+			}
+
+			return 'Please enter a valid first and last name.';
+		},
+	},
+    {
+		//Id
+		type: 'input',
+		message: 'Enter employee ID number',
+		name: 'id',
+		validate: function(id) {
+			let pass = id.match(/\S+@\S+\.\S+/g);
+			if (pass) {
+				return true;
+			}
+
+			return 'Please enter a valid ID number.';
+		},
+	},
+	{
+		//Email
+		type: 'input',
+		message: 'Enter your email',
+		name: 'email',
+		validate: function(email) {
+			if (username) {
+				return true;
+			}
+
+			return 'Please enter a valid email';
+		},
+	},
+		
+];
+
+module.exports = employee;
